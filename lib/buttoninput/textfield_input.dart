60f95ca9f3aa68ab7e8f91b65b1f20e3038eaee6
GulajavaMinistudio <gulajava.mini@gmail.com>
2022-06-20 04:35:11 +0700

    Latihan pengenalan widget input dan images

--- lib/buttoninput/textfield_input.dart
+++ lib/buttoninput/textfield_input.dart
@@ -20,13 +20,42 @@ class TextfieldInputs extends StatelessWidget {
         ),
       ),
       body: SafeArea(
-        child: SingleChildScrollView(
-          child: Container(
-            margin: const EdgeInsets.all(14),
-            child: Column(
-              children: const [
-                EditingTextSampel(),
-              ],
+        child: Scrollbar(
+          thumbVisibility: true,
+          trackVisibility: true,
+          child: SingleChildScrollView(
+            child: Container(
+              margin: const EdgeInsets.all(14),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: const [
+                  EditingTextSampel(),
+                  EditingTextCtrl(),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Text('Pengenalan Switch Widget'),
+                  SwitchWidgetSampel(),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Text('Sampel Radio Button'),
+                  RadioButtonSampel(),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Text('Sampel Checkbox Button'),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  CheckboxSampels(),
+                  Text('Sampel Image'),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  ImageSampel(),
+                ],
+              ),
             ),
           ),
         ),
@@ -77,32 +106,33 @@ class _EditingTextSampelState extends State<EditingTextSampel> {
             child: ElevatedButton(
               onPressed: () {
                 showDialog(
-                    barrierDismissible: false,
-                    context: context,
-                    builder: (context) {
-                      return AlertDialog(
-                        title: const Text('Dialog Edit'),
-                        content: Text(
-                            'Halo si nama orang $_nameOrang dan final nama orang $_nameOrangFinal'),
-                        actions: [
-                          TextButton(
-                            onPressed: () {
-                              Navigator.of(context).pop();
-                            },
-                            child: const Text('Yes'),
+                  barrierDismissible: false,
+                  context: context,
+                  builder: (context) {
+                    return AlertDialog(
+                      title: const Text('Dialog Controller Input'),
+                      content: Text(
+                          'Halo si nama orang $_nameOrang dan final nama orang $_nameOrangFinal'),
+                      actions: [
+                        TextButton(
+                          onPressed: () {
+                            Navigator.of(context).pop();
+                          },
+                          child: const Text('Yes'),
+                        ),
+                        TextButton(
+                          onPressed: () {
+                            Navigator.of(context).pop();
+                          },
+                          child: const Text(
+                            'Tidak',
+                            style: TextStyle(color: Colors.red),
                           ),
-                          TextButton(
-                            onPressed: () {
-                              Navigator.of(context).pop();
-                            },
-                            child: const Text(
-                              'Tidak',
-                              style: TextStyle(color: Colors.red),
-                            ),
-                          )
-                        ],
-                      );
-                    });
+                        )
+                      ],
+                    );
+                  },
+                );
               },
               style: TextButton.styleFrom(
                   backgroundColor: Colors.green,
@@ -110,7 +140,7 @@ class _EditingTextSampelState extends State<EditingTextSampel> {
                     color: Colors.white,
                     fontWeight: FontWeight.bold,
                   )),
-              child: const Text('Cek Input 1'),
+              child: const Text('Submit Input 2'),
             ),
           )
         ],
@@ -146,14 +176,240 @@ class _EditingTextCtrlState extends State<EditingTextCtrl> {
       margin: const EdgeInsets.only(top: 16),
       child: Column(
         children: [
+          // Input tipe password
+          // https://stackoverflow.com/questions/52111853/how-to-add-a-password-input-type-in-flutter-makes-the-password-user-input-is-not
           TextField(
-            controller: editingControllers,
-            decoration: const InputDecoration(
-              labelText: 'Sampel Input 2',
-              hintText: 'Password',
+              obscureText: false,
+              enableSuggestions: false,
+              autocorrect: false,
+              autofillHints: const ['password'],
+              controller: editingControllers,
+              decoration: const InputDecoration(
+                labelText: 'Sampel Input 2',
+                hintText: 'Password',
+              ),
+              keyboardType: TextInputType.visiblePassword),
+          const SizedBox(
+            height: 10,
+          ),
+          Align(
+            alignment: Alignment.topRight,
+            child: ElevatedButton(
+              onPressed: () {
+                showDialog(
+                    barrierDismissible: false,
+                    context: context,
+                    builder: (context) {
+                      return AlertDialog(
+                        title: const Text('Dialog Edit'),
+                        content: Text(
+                            'Halo si nama orang ${editingControllers.text} dan final nama orang ${editingControllers.text}'),
+                        actions: [
+                          TextButton(
+                            onPressed: () {
+                              Navigator.of(context).pop();
+                            },
+                            child: const Text('Yes'),
+                          ),
+                          TextButton(
+                            onPressed: () {
+                              Navigator.of(context).pop();
+                            },
+                            child: const Text(
+                              'Tidak',
+                              style: TextStyle(color: Colors.red),
+                            ),
+                          )
+                        ],
+                      );
+                    });
+              },
+              style: TextButton.styleFrom(
+                backgroundColor: Colors.green,
+                textStyle: const TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              child: const Text('Cek Input Ctrl'),
             ),
           )
         ],
+      ),
+    );
+  }
+}
+
+class SwitchWidgetSampel extends StatefulWidget {
+  const SwitchWidgetSampel({Key? key}) : super(key: key);
+
+  @override
+  State<SwitchWidgetSampel> createState() => _SwitchWidgetSampelState();
+}
+
+class _SwitchWidgetSampelState extends State<SwitchWidgetSampel> {
+  bool switchON = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.only(
+        top: 16,
+      ),
+      child: Column(
+        children: [
+          const Text('Switch On Of'),
+          Switch(
+            value: switchON,
+            onChanged: (bool isValueOk) {
+              setState(() {
+                switchON = isValueOk;
+              });
+
+              ScaffoldMessenger.of(context).showSnackBar(
+                SnackBar(
+                  content:
+                      Text(switchON ? 'IsValue OK true' : 'IsValue OK false'),
+                  duration: const Duration(seconds: 2),
+                ),
+              );
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class RadioButtonSampel extends StatefulWidget {
+  const RadioButtonSampel({Key? key}) : super(key: key);
+
+  @override
+  State<RadioButtonSampel> createState() => _RadioButtonSampelState();
+}
+
+class _RadioButtonSampelState extends State<RadioButtonSampel> {
+  String? languageProgram = '';
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.only(top: 20),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          // Dokumentasi ListTile
+          // https://api.flutter.dev/flutter/material/ListTile-class.html
+          ListTile(
+            leading: Radio(
+              value: 'Dart',
+              groupValue: languageProgram,
+              onChanged: (String? stringvalue) {
+                if (stringvalue != null) {
+                  setState(() {
+                    languageProgram = stringvalue;
+                  });
+                  showSnackbar();
+                }
+              },
+            ),
+            title: const Text('Dart'),
+          ),
+          ListTile(
+            leading: Radio(
+              value: 'Kotlin',
+              groupValue: languageProgram,
+              onChanged: (String? stringvalue) {
+                if (stringvalue != null) {
+                  setState(() {
+                    languageProgram = stringvalue;
+                  });
+                  showSnackbar();
+                }
+              },
+            ),
+            title: const Text('Kotlin'),
+          ),
+          ListTile(
+            leading: Radio(
+              value: 'JavaScript',
+              groupValue: languageProgram,
+              onChanged: (String? stringvalue) {
+                if (stringvalue != null) {
+                  setState(() {
+                    languageProgram = stringvalue;
+                  });
+                  showSnackbar();
+                }
+              },
+            ),
+            title: const Text('JavaScript'),
+          )
+        ],
+      ),
+    );
+  }
+
+  void showSnackbar() {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text('$languageProgram dipilih'),
+        duration: const Duration(seconds: 1),
+      ),
+    );
+  }
+}
+
+class CheckboxSampels extends StatefulWidget {
+  const CheckboxSampels({Key? key}) : super(key: key);
+
+  @override
+  State<CheckboxSampels> createState() => _CheckboxSampelsState();
+}
+
+class _CheckboxSampelsState extends State<CheckboxSampels> {
+  bool isOkSetuju = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.only(top: 12),
+      child: Column(
+        mainAxisSize: MainAxisSize.max,
+        children: [
+          ListTile(
+            leading: Checkbox(
+              value: isOkSetuju,
+              onChanged: (bool? isOkValue) {
+                if (isOkValue != null) {
+                  setState(() {
+                    isOkSetuju = isOkValue;
+                    print(isOkSetuju);
+                  });
+                }
+              },
+            ),
+            title: const Text('Setuju atau Tidak ?'),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class ImageSampel extends StatelessWidget {
+  const ImageSampel({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.only(top: 20),
+      child: Center(
+        child: Image.network(
+          'https://picsum.photos/200/300',
+          width: 200,
+          height: 200,
+        ),
       ),
     );
   }
