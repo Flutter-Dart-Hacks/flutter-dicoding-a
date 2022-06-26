32ac1722b140babdd93ce936d5a7b08712f651e5
GulajavaMinistudio <gulajava.mini@gmail.com>
2022-06-27 06:31:48 +0700

    Latihan pengenalan list

---
+++ lib/lists/list_data.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class ListDataSampel extends StatelessWidget {
+  const ListDataSampel({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
+class ListBiasa extends StatefulWidget {
+  const ListBiasa({Key? key}) : super(key: key);
+
+  @override
+  State<ListBiasa> createState() => _ListBiasaState();
+}
+
+class _ListBiasaState extends State<ListBiasa> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
